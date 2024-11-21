becc728b630ec64f9c5aae269290bc0eab1702bd
GumarovDamir <144334963+GumarovDamir@users.noreply.github.com>
2024-11-21 23:43:22 +0500

    Add files via upload

---
+++ Homework/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace less
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Сегодня 2024 год";
+            string result = ExtractDigits(text);
+            Console.WriteLine(result);
+        }
+        static string ExtractDigits(string text)
+        {
+            return new string(text.Where(char.IsDigit).ToArray());
+        }
+    }
+}
