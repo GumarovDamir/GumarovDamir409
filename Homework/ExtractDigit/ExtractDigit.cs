5dcdccc219395513a4eacd8771554d0fe8ea7057
Gumarov Damir <damir.gumarov.123@mail.ru>
2024-11-26 22:55:27 +0500

    Create ExtractDigit.cs

---
+++ Homework/ExtractDigit/ExtractDigit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.ExtractDigit
+{
+    internal class ExtractDigit
+    {
+        static void Main(string[] args)
+        {
+            string input = "Сейчас 2024 год, 21 век";
+            string result = ExtractDigits(input);
+            Console.WriteLine(result);
+        }
+        static string ExtractDigits(string input)
+        {
+            string digits = string.Empty;
+
+            foreach (char c in input)
+            {
+                if (char.IsDigit(c))
+                {
+                    digits += c;
+                }
+            }
+            return digits;
+        }
+    }
+}
