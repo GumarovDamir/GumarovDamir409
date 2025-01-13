f9c1576055d61bfa922803396ef8ed80ada9cee2
Gumarov Damir <damir.gumarov.123@mail.ru>
2025-01-13 11:56:16 +0500

    Create Capitalization.cs

---
+++ Homework/Capitalization/Capitalization.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.Capitalization
+{
+    internal class Capitalization
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            string input = Console.ReadLine();
+            string result = CapitalizationOfTheText(input);
+            Console.WriteLine(result);
+
+        }
+        static string CapitalizationOfTheText(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return input;
+            }
+            string[] words = input.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > 0)
+                {
+                    words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1).ToLower();
+                }
+            }
+            return string.Join(" ", words);
+        }
+    }
+}
